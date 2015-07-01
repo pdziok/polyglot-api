347af289ef7adde618f379762ab508f3f65cba3c
Pawel Dziok <pawel.dziok@schibsted.pl>
2015-07-01 10:25:00 +0200

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, abort
+from polyglot.text import Text
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['POST'])
+def ner():
+    if not request.json or not 'text' in request.json:
+        abort(400)
+
+    text = Text(request.json['text'])
+    entities = text.entities
+    result = {}
+    for entity in entities:
+        if entity.tag not in result:
+            result[entity.tag] = set()
+
+        result[entity.tag].add(' '.join(entity))
+        print(entity.tag, entity)
+
+    for tagType in result:
+        result[tagType] = list(result[tagType])
+
+    return jsonify(result), 200
+
+@app.route('/')
+def index():
+    return jsonify({'error': 'Use /search endpoint'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
